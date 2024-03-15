068574e17749f1e97621ea05ae2282512becbe80
Karl Johannisson <karl.johannisson+github2@gmail.com>
2024-03-15 16:17:43 +0100

    add children to value struct

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use crate::value::Value;
 
 
 fn main() {
-    let mut a = Value::new(10f64);
-    let mut b = Value::new(3f64);
+    let mut a = Value::new(10f64, None);
+    let mut b = Value::new(3f64, None);
 
     println!("value data is: {}", a + b)
 }
--- src/value.rs
+++ src/value.rs
@@ -1,13 +1,16 @@
 use std::{fmt, ops};
+use std::rc::Rc;
 
 pub struct Value {
     data: f64,
+    children: Option<(Rc<Value>, Rc<Value>)>,
 }
 
 impl Value {
-    pub fn new(data: f64) -> Value {
+    pub fn new(data: f64, children: Option<(Rc<Value>, Rc<Value>)>) -> Value {
         Value {
-            data: data,
+            data,
+            children,
         }
     }
 }
@@ -15,14 +18,14 @@ impl Value {
 impl ops::Add<Value> for Value {
     type Output = Value;
     fn add(self, _rhs: Value) -> Value {
-        Value::new(self.data + _rhs.data)
+        Value::new(self.data + _rhs.data, Some((Rc::new(self), Rc::new(_rhs))))
     }
 }
 
 impl ops::Mul<Value> for Value {
     type Output = Value;
     fn mul(self, _rhs: Value) -> Value {
-        Value::new(self.data * _rhs.data)
+        Value::new(self.data * _rhs.data, Some((Rc::new(self), Rc::new(_rhs))))
     }
 }
 
@@ -40,16 +43,16 @@ mod tests {
 
     #[test]
     fn test_add() {
-        let mut a = Value::new(10f64);
-        let mut b = Value::new(3f64);
+        let mut a = Value::new(10f64, None);
+        let mut b = Value::new(3f64, None);
         let c=a+b;
         assert_eq!(c.data, 13f64);
     }
 
     #[test]
     fn test_mul() {
-        let mut a = Value::new(10f64);
-        let mut b = Value::new(3f64);
+        let mut a = Value::new(10f64, None);
+        let mut b = Value::new(3f64, None);
         let c=a*b;
         assert_eq!(c.data, 30f64);
     }
