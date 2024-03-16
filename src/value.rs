df80fb19e4ca669cb53d1bd5098620b3b857a29b
Karl Johannisson <karl.johannisson+github2@gmail.com>
2024-03-16 20:18:45 +0100

    add opcode

--- src/value.rs
+++ src/value.rs
@@ -5,42 +5,51 @@ pub struct Value {
     data: f64,
     child_a: Option<Box<Value>>,
     child_b: Option<Box<Value>>,
+    op: char,
 }
 
 impl Value {
-    pub fn new(data: f64, child_a: Option<Value>, child_b: Option<Value>) -> Value {
+    pub fn new(data: f64) -> Value {
         Value {
             data,
+            child_a: None,
+            child_b: None,
+            op: ' ',
+
+        }
+    }
+    pub fn new_from_op(data:f64, child_a: Option<Value>, child_b: Option<Value>, op: char)
+        -> Value {
+            Value {
+            data,
             child_a: match child_a {
-                           None => None,
-                           Some(child_a) =>
-                               Some(Box::new(child_a)),
-                              },
+                None => None,
+                Some(child_a) =>
+                    Some(Box::new(child_a)),
+            },
             child_b: match child_b {
                 None => None,
                 Some(child_b) =>
                     Some(Box::new(child_b)),
             },
+            op,
         }
     }
+
 }
 
 impl Add for Value {
     type Output = Self;
     fn add(self, rhs: Self) -> Self {
-        Value::new(self.data + &rhs.data, Some(self), Some(rhs))
-
+        Value::new_from_op(self.data + rhs.data, Some(self), Some(rhs), '+')
     }
 }
-
 impl Mul for Value {
     type Output = Self;
     fn mul(self, rhs: Self) -> Self {
-        Value::new(self.data * &rhs.data, Some(self), Some(rhs))
-
+        Value::new_from_op(self.data * rhs.data, Some(self), Some(rhs), '*')
     }
 }
-
 
 
 impl fmt::Display for Value {
@@ -57,28 +66,44 @@ mod tests {
 
     #[test]
     fn test_add() {
-       let a = Value::new(10f64, None, None);
-       let b = Value::new(3f64, None, None);
+       let a = Value::new(10f64);
+       let b = Value::new(3f64);
        let c=a+b;
        assert_eq!(c.data, 13f64);
     }
 
     #[test]
     fn test_mul() {
-        let a = Value::new(2.5f64, None, None);
-        let b = Value::new(5f64, None, None);
+        let a = Value::new(2.5f64);
+        let b = Value::new(5f64);
         let c=a*b;
         assert_eq!(c.data, 12.5f64);
     }
 
     #[test]
     fn test_children() {
-        let a = Value::new(10f64, None, None);
-        let b = Value::new(3f64, None, None);
+        let a = Value::new(10f64);
+        let b = Value::new(3f64);
         let c=a+b;
         assert_eq!(c.child_a.unwrap().data, 10f64);
         assert_eq!(c.child_b.unwrap().data, 3f64);
 
+    }
+
+    #[test]
+    fn test_add_op() {
+        let a = Value::new(2.5f64);
+        let b = Value::new(5f64);
+        let c=a+b;
+        assert_eq!(c.op, '+');
+    }
+
+    #[test]
+    fn test_mul_op() {
+        let a = Value::new(2.5f64);
+        let b = Value::new(5f64);
+        let c=a*b;
+        assert_eq!(c.op, '*');
     }
 
 }
